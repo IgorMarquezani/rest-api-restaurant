925c38b6c171cfca900249ba1be6feb0263acf46
IgorMarquezani <igormarquezani@gmail.com>
2023-03-02 08:56:31 -0300

    adding bash script for running the application

--- containers-files/start.sh
+++ containers-files/start.sh
@@ -9,18 +9,18 @@ elif [[ $1 == "reset" ]]; then
     container=$(docker image ls | sed '2,800 ! d' | sed -n {"$i p"} | awk {'print $1'} )
     if [[ $container == "postgres" || $container == "dpage/pgadmin4" ]]; then
       docker image rmi -f $container
-      $i=$(($i-1))
+      i=$(($i-1))
     fi
   done
   docker volume rm $(docker volume ls -q) 2> /dev/null 
 elif [[ $1 == "help" ]]; then
   if [[ $LANG == "pt_BR.UTF-8" ]]; then
-    echo "help:  show this message"
-    echo "start: start the containers"
-    echo "reset: completely reset containers, images and volumes"
-  fi
+    echo "help:  mostra esta mensagem"
+    echo "start: inicia os containers"
+    echo "reset: reinicia totalmente containers, imagens e volumes"
   else
-  echo "help:  mostra esta mensagem"
-  echo "start: inicia os containers"
-  echo "reset: reinicia totalmente containers, imagens e volumes"
+  echo "help:  show this message"
+  echo "start: start the containers"
+  echo "reset: completely reset containers, images and volumes"
+  fi
 fi
